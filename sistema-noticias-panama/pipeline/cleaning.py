71261aee66ecef6388773d5f90e233d065ab9d0e
Jemanuel514 <jemanuel0123@hotmail.com>
2026-06-12 01:04:17 -0500

    chore: add logging for testing in cleaning module

--- sistema-noticias-panama/pipeline/cleaning.py
+++ sistema-noticias-panama/pipeline/cleaning.py
@@ -26,9 +26,11 @@ if __name__ == "__main__":
         f"{Path(__file__).parent}/../data/raw/noticias_tvn.csv"
     ]
 
-    df = pd.concat(
-        [clean_data(pd.read_csv(source)) for source in sources],
-        ignore_index=True
-    )
+    processed_data = []
+    for source in sources:
+        print(f"Processing: {source}")
+        processed_data.append(clean_data(pd.read_csv(source)))
+
+    df = pd.concat(processed_data, ignore_index=True)
     df.drop_duplicates(subset=["titulo"], inplace=True)
     df.to_csv(f"{Path(__file__).parent}/../data/processed/noticias_panama_procesadas.csv", index=False)
