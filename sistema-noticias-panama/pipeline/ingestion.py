81a190341e4e9a5b0ea520b420f67a8a5c3f1b33
Jemanuel514 <jemanuel0123@hotmail.com>
2026-06-12 19:47:43 -0500

    refactor: refactor ingestion module

--- sistema-noticias-panama/pipeline/ingestion.py
+++ sistema-noticias-panama/pipeline/ingestion.py
@@ -1,36 +1,56 @@
+from . import pd, Path
+from .commons import save_to_csv
 from scrapers import scrap_tvn_news_section
-import pandas as pd
-from pathlib import Path
 
 
-def scrap_news(scraper: function, sources: list) -> list:
-    news_data = []
+def scrape_sources(sources: list[dict], scraper: function) -> list[dict]:
+    scraped_data = []
     for source in sources:
-        news_data.extend(scraper(source))
-    return news_data
+        print(f"Scraping: {source['url']}")
+        source_data = scraper(source["url"], source["pages"])
+        scraped_data.extend(source_data)
+    return scraped_data
 
 
-if __name__ == "__main__":
-    configs = [
+def ingest_data(ingestion_configs: list[dict]) -> list[str]:
+    outputs: list[str] = []
+    for ingestion in ingestion_configs:
+        if ingestion["type"] == "web_scraping":
+            print(f"{'='*25} WEB SCRAPING PROCESS {'='*25}")
+            scraping = ingestion["config"]
+
+            data = scrape_sources(scraping["sources"], scraping["scraper"])
+            df = pd.DataFrame(data)
+            save_to_csv(df, scraping["output"])
+            outputs.append(scraping["output"])
+    
+    return outputs
+
+
+def main():
+    FILE_DIR = Path(__file__).parent
+    INGESTION_CONFIGS = [
         {
-            "scraper": scrap_tvn_news_section,
-            "sources": [
-                "https://www.tvn-2.com/nacionales/",
-                "https://www.tvn-2.com/mundo/",
-                "https://www.tvn-2.com/contenido-exclusivo/",
-                "https://www.tvn-2.com/entretenimiento/",
-                "https://www.tvn-2.com/tvmax/lpf/",
-                "https://www.tvn-2.com/tvmax/futbol-internacional/",
-                "https://www.tvn-2.com/tvmax/beisbol-nacional/",
-                "https://www.tvn-2.com/tvmax/beisbol/",
-                "https://www.tvn-2.com/tvmax/mas-deportes/"
-            ],
-            "output":f"{Path(__file__).parent}/../data/raw/noticias_tvn.csv"
+            "type": "web_scraping",
+            "config": {
+                "scraper": scrap_tvn_news_section,
+                "output": f"{FILE_DIR}/../data/raw/noticias_tvn.csv",
+                "sources": [
+                    {"url": "https://www.tvn-2.com/nacionales/",                 "pages": 1},
+                    {"url": "https://www.tvn-2.com/mundo/",                      "pages": 1},
+                    {"url": "https://www.tvn-2.com/contenido-exclusivo/",        "pages": 1},
+                    {"url": "https://www.tvn-2.com/entretenimiento/",            "pages": 1},
+                    {"url": "https://www.tvn-2.com/tvmax/lpf/",                  "pages": 1},
+                    {"url": "https://www.tvn-2.com/tvmax/futbol-internacional/", "pages": 1},
+                    {"url": "https://www.tvn-2.com/tvmax/beisbol-nacional/",     "pages": 1},
+                    {"url": "https://www.tvn-2.com/tvmax/beisbol/",              "pages": 1},
+                    {"url": "https://www.tvn-2.com/tvmax/mas-deportes/",         "pages": 1}
+                ]
+            }
         }
     ]
+    
+    ingest_data(INGESTION_CONFIGS)
 
-    for config in configs:
-        news_data = scrap_news(config["scraper"], config["sources"])
-        df = pd.DataFrame(news_data)
-        df.to_csv(config["output"], index=False)
-        print(f"Scraped {config["output"].split("/")[-1]}")
+if __name__ == "__main__":
+    main()
