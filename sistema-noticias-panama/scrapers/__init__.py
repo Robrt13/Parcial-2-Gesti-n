805f6e7d652fae76188faa68c339767f099e3e6a
Jemanuel514 <jemanuel0123@hotmail.com>
2026-06-15 20:39:24 -0500

    feat: add save_to_csv to module functions

--- sistema-noticias-panama/scrapers/__init__.py
+++ sistema-noticias-panama/scrapers/__init__.py
@@ -1,3 +1,4 @@
+from .commons import save_to_csv
 from .scraper_tvn import scrap_tvn_news_section, CATEGORIAS as CATEGORIAS_tvn
 from .scraper_telemetro import scrap_telemetro_news_section, CATEGORIAS as CATEGORIAS_tlm
-from .scraper_laprensa import scrap_laprensa_news_section
+from .scraper_laprensa import scrap_laprensa_news_section, CATEGORIAS as CATEGORIAS_lp
